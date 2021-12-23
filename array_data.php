99cd7bf49205746ff364827c107bce40668bd0f0
akirayuu <fatinilidya@gmail.com>
2021-12-23 16:04:24 +0800

    update array test with array data

---
+++ array_data.php
@@ -0,0 +1,24 @@
+<?php
+//create an array list of data
+$data = '[
+    [
+    "name" => "Aquila Macias",
+    "phone" => "(115) 773-5464",
+    "email" => "metus.sit@hotmail.couk"
+],
+[
+    "name" => "Aspen Kennedy",
+    "phone" => "(115) 756-0987",
+    "email" => "aspen@gmail.com"
+],
+[
+    "name" => "Ora Harrison",
+    "phone" => "(115) 234-5467",
+    "email" => "ora@yahoo.com"
+]
+]';
+//print out the data
+//echo 'no error';
+print_r($data);
+
+?>
